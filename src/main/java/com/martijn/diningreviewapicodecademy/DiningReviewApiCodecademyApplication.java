fc3860d744fb24805f825494ec4786b5b9630cbe
MartijnvCitteren <141489106+MartijnvCitteren@users.noreply.github.com>
2023-12-18 11:06:30 +0100

    configured H2 DB

---
+++ src/main/java/com/martijn/diningreviewapicodecademy/DiningReviewApiCodecademyApplication.java
@@ -0,0 +1,13 @@
+package com.martijn.diningreviewapicodecademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningReviewApiCodecademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningReviewApiCodecademyApplication.class, args);
+	}
+
+}
